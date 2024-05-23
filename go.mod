55e1f0629025ff53a92735c1633e41fca1846ec8
hodadis <hodadisbirhan80@gmail.com>
2024-05-23 18:16:13 +0300

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module Go-server/index
+
+go 1.22.3
---
+++ index.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	fmt.Println("This is main Function")
+	Wellcome()
+
+}
+
+func Wellcome() {
+
+	fmt.Println("Well Come To Go Programming Language")
+
+}
